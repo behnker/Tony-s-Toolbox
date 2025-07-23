a721c56e8daefb462c943a7fd3d01050855df3db
Ralph Behnke <ralph.behnke91@gmail.com>
2025-07-23 13:26:58 +0000

    Error: Could not parse module '[project]/src/lib/firebase/server.ts'
    Un

--- src/lib/firebase/server.ts
+++ src/lib/firebase/server.ts
@@ -1,26 +1,10 @@
-import { initializeApp, getApps, getApp, cert } from "firebase-admin/app";
+
+import { initializeApp, getApps, getApp, cert, type ServiceAccount } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import path from 'path';
-import fs from 'fs';
+import serviceAccountJson from '../../../serviceAccountKey.json';
 
-function getServiceAccount() {
-    const keyPath = path.resolve(process.cwd(), 'serviceAccountKey.json');
-
-    if (!fs.existsSync(keyPath)) {
-        throw new Error('serviceAccountKey.json is not found. Please add it to the root of your project. Make sure to add it to .gitignore as well.');
-    }
-
-    const serviceAccountString = fs.readFileSync(keyPath, 'utf8');
-    const serviceAccount = JSON.parse(serviceAccountString);
-
-    if (serviceAccount.private_key) {
-        serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
-    }
-    
-    return serviceAccount;
-}
-
-const serviceAccount = getServiceAccount();
+// Cast the imported JSON to the ServiceAccount type.
+const serviceAccount = serviceAccountJson as ServiceAccount;
 
 const app = !getApps().length ? initializeApp({
     credential: cert(serviceAccount)
