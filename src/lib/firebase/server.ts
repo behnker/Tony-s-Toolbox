4f51e6935bf54aea80bc6d9e3ce719798c1bf7ef
Ralph Behnke <ralph.behnke91@gmail.com>
2025-07-23 21:03:35 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase/server.ts
+++ src/lib/firebase/server.ts
@@ -1,10 +1,15 @@
-
 import { initializeApp, getApps, getApp, cert, type ServiceAccount } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import serviceAccountJson from '../../../serviceAccountKey.json';
 
 // Cast the imported JSON to the ServiceAccount type.
 const serviceAccount = serviceAccountJson as ServiceAccount;
+
+// The `private_key` from the file might have its newlines escaped as "\\n".
+// The `cert` function needs the private key to have literal newlines.
+if (serviceAccount.private_key) {
+    serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+}
 
 const app = !getApps().length ? initializeApp({
     credential: cert(serviceAccount)
