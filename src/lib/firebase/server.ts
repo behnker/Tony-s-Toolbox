f7114ff726e8fbcd93299437756d54158b0a928f
Ralph Behnke <ralph.behnke91@gmail.com>
2025-07-23 11:35:38 +0000

    SyntaxError: Bad control character in string literal in JSON at position

--- src/lib/firebase/server.ts
+++ src/lib/firebase/server.ts
@@ -7,8 +7,10 @@ if (!process.env.FIREBASE_SERVICE_ACCOUNT_KEY) {
     throw new Error('FIREBASE_SERVICE_ACCOUNT_KEY is not set. Please add it to your .env.local file.');
 }
 
-const serviceAccountString = (process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string).replace(/\\n/g, '\\\\n');
+// Sanitize the service account key by removing invalid control characters
+const serviceAccountString = (process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string).replace(/\\n/g, "\\n");
 const serviceAccount = JSON.parse(serviceAccountString);
+
 
 const app = !getApps().length ? initializeApp({
     credential: cert(serviceAccount)
