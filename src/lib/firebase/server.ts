f093a0560647340a8a6d358ca7bee6fde1ed57f6
Ralph Behnke <ralph.behnke91@gmail.com>
2025-07-23 22:18:05 +0000

    Error: Failed to parse private key: Error: Invalid PEM formatted message

--- src/lib/firebase/server.ts
+++ src/lib/firebase/server.ts
@@ -2,11 +2,8 @@ import { initializeApp, getApps, getApp, cert, type ServiceAccount } from "fireb
 import { getFirestore } from "firebase-admin/firestore";
 import serviceAccountJson from '../../../serviceAccountKey.json';
 
-// Cast the imported JSON to the ServiceAccount type.
 const serviceAccount = serviceAccountJson as ServiceAccount;
 
-// The `private_key` from the file might have its newlines escaped as "\\n".
-// The `cert` function needs the private key to have literal newlines.
 if (serviceAccount.private_key) {
     serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 }
